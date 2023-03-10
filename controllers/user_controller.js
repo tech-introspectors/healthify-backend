c62e3b53075836bf8fac8584aef0dc0b73b2081f
mg143pavankumar <pavansharma.mg143@gmail.com>
2023-03-11 01:53:05 +0530

    [added]: user logout api

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -42,3 +42,15 @@ exports.loginUser = catchAsyncError(async (req, res, next) => {
 
   sendToken(user, 200, res, "User login successsfully");
 });
+
+exports.logoutUser = catchAsyncError(async (req, res, next) => {
+  res.cookie("token", null, {
+    expires: new Date(Date.now()),
+    httpOnly: true,
+  });
+
+  res.status(200).json({
+    success: true,
+    message: "Logged out",
+  });
+});
--- routes/user_routes.js
+++ routes/user_routes.js
@@ -1,9 +1,14 @@
 const express = require("express");
-const { registerUser, loginUser } = require("../controllers/user_controller");
+const {
+  registerUser,
+  loginUser,
+  logoutUser,
+} = require("../controllers/user_controller");
 const route = express.Router();
 
 // users related routes
 route.route("/user-register").post(registerUser);
 route.route("/user-login").post(loginUser);
+route.route("/user-logout").post(logoutUser);
 
 module.exports = route;
