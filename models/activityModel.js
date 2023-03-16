6a5a030dce2712890a65f81dd9045b3ff4bfa569
mg143pavankumar <pavansharma.mg143@gmail.com>
2023-03-16 19:51:04 +0530

    update

--- models/activityModel.js
+++ models/activityModel.js
@@ -5,44 +5,41 @@ const jwt = require("jsonwebtoken");
 const crypto = require("crypto");
 const { url } = require("inspector");
 
-  const activitySchema = new mongoose.Schema({
+const activitySchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
 
-    user_id: { 
-      type: Number, 
-      required: true 
-    },
-    
-    date: { 
-      type: Date, 
-      required: true 
-    },
-    
-    steps: { 
-      type: Number, 
-      required: true 
-    },
-    
-    distance: { 
-      type: Number, 
-      required: true 
-    },
-    
-    runDistance: { 
-      type: Number, 
-      required: true 
-    },
-    
-    calories: { 
-      type: Number, 
-      required: true 
-    },
-    
-    activityType: { 
-      type: String, 
-      required: true 
-    },
- 
+  steps: {
+    type: Number,
+    required: true,
+  },
+
+  distance: {
+    type: Number,
+    required: true,
+  },
+
+  runDistance: {
+    type: Number,
+    required: true,
+  },
+
+  calories: {
+    type: Number,
+    required: true,
+  },
+
+  activityType: {
+    type: String,
+    required: true,
+  },
 });
-
 
 module.exports = mongoose.model("Activity", activitySchema);
