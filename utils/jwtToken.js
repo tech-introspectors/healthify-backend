effaff36ba4f955f77153ee8561757a8be577b8a
mg143pavankumar <pavansharma.mg143@gmail.com>
2023-03-16 21:01:21 +0530

    updated jwtToken file

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -4,12 +4,8 @@ const sendToken = (user, statusCode, res, msg) => {
 
   // options for cookies
   const options = {
-    expires: new Date(
-      Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
-    ),
+    expires: new Date(Date.now() + 30 * 24 * 3600000),
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "none",
   };
 
   res.status(statusCode).cookie("token", token, options).json({
